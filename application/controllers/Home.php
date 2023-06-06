6b502f5394f107466d89b7b9b49638b7d4a6737c
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-06 10:36:05 +0700

    Project baru

---
+++ application/controllers/Home.php
@@ -0,0 +1,36 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Load Dependencies
+
+	}
+
+	// List all your items
+	public function index()
+	{
+	}
+
+	// Add a new item
+	public function add()
+	{
+	}
+
+	//Update one item
+	public function update($id = NULL)
+	{
+	}
+
+	//Delete one item
+	public function delete($id = NULL)
+	{
+	}
+}
+
+/* End of file Home.php */
