fdc6c1fead1c93c881955aed4ea0ff9adc428ef6
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-15 15:23:16 +0700

    view frontend

---
+++ application/views/frontend/v_head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>Sabujcha - Matcha eCommerce Bootstrap4 Template</title>
+	<meta name="description" content="">
+	<meta name="robots" content="noindex, follow" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="<?= base_url('frontend/') ?>assets/img/favicon.png">
+
+	<!-- all css here -->
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/animate.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/slick.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/chosen.min.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/themify-icons.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/ionicons.min.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/jquery-ui.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/meanmenu.min.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/style.css">
+	<link rel="stylesheet" href="<?= base_url('frontend/') ?>assets/css/responsive.css">
+	<script src="<?= base_url('frontend/') ?>assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
