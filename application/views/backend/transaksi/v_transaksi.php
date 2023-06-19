d0d068bec5c81ffcacc3920a1db4ad1111cfe865
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-19 11:17:46 +0700

    view transaksi

---
+++ application/views/backend/transaksi/v_transaksi.php
@@ -0,0 +1,143 @@
+<div class="main-panel">
+	<div class="content-wrapper">
+		<div class="row">
+			<div class="card card-primary card-outline">
+				<div class="card-header">
+					<h3 class="card-title">
+						<i class="fas fa-edit"></i>
+						<?= $title ?>
+					</h3>
+				</div>
+				<div class="card-body">
+					<h4>Transaksi</h4>
+					<div class="row">
+						<div class="col-5 col-sm-3">
+							<div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
+								<a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="true">Masuk</a>
+								<a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">Proses</a>
+								<a class="nav-link" id="vert-tabs-messages-tab" data-toggle="pill" href="#vert-tabs-messages" role="tab" aria-controls="vert-tabs-messages" aria-selected="false">Kirim</a>
+								<a class="nav-link" id="vert-tabs-settings-tab" data-toggle="pill" href="#vert-tabs-settings" role="tab" aria-controls="vert-tabs-settings" aria-selected="false">Selesai</a>
+							</div>
+						</div>
+						<div class="col-7 col-sm-9">
+							<div class="tab-content" id="vert-tabs-tabContent">
+								<div class="tab-pane text-left fade show active" id="vert-tabs-home" role="tabpanel" aria-labelledby="vert-tabs-home-tab">
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th scope="col">#</th>
+												<th scope="col">No Pemesanan</th>
+												<th scope="col">Harga Tiket</th>
+												<th scope="col">Qty</th>
+												<th scope="col">Tanggal Booking</th>
+												<th scope="col">Metode Bayar</th>
+												<th scope="col">Status Bayar/Pemesanan</th>
+												<th scope="col">Setting</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								<div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel" aria-labelledby="vert-tabs-profile-tab">
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th scope="col">#</th>
+												<th scope="col">No Pemesanan</th>
+												<th scope="col">Harga Tiket</th>
+												<th scope="col">Qty</th>
+												<th scope="col">Tanggal Booking</th>
+												<th scope="col">Metode Bayar</th>
+												<th scope="col">Status Bayar/Pemesanan</th>
+												<th scope="col">Setting</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								<div class="tab-pane fade" id="vert-tabs-messages" role="tabpanel" aria-labelledby="vert-tabs-messages-tab">
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th scope="col">#</th>
+												<th scope="col">No Pemesanan</th>
+												<th scope="col">Harga Tiket</th>
+												<th scope="col">Qty</th>
+												<th scope="col">Tanggal Booking</th>
+												<th scope="col">Metode Bayar</th>
+												<th scope="col">Status Bayar/Pemesanan</th>
+												<th scope="col">Setting</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+								<div class="tab-pane fade" id="vert-tabs-settings" role="tabpanel" aria-labelledby="vert-tabs-settings-tab">
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th scope="col">#</th>
+												<th scope="col">No Pemesanan</th>
+												<th scope="col">Harga Tiket</th>
+												<th scope="col">Qty</th>
+												<th scope="col">Tanggal Booking</th>
+												<th scope="col">Metode Bayar</th>
+												<th scope="col">Status Bayar/Pemesanan</th>
+												<th scope="col">Setting</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+												<td></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /.card -->
+			</div>
+		</div>
+	</div>
