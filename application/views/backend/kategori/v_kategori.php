3bb2397f79190f686d6c50643cb26d2fc1e89a19
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-19 11:18:16 +0700

    view kategori

---
+++ application/views/backend/kategori/v_kategori.php
@@ -0,0 +1,129 @@
+<div class="main-panel">
+	<div class="content-wrapper">
+		<div class="row">
+			<div class="col-lg-12 grid-margin stretch-card">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="card-title"><?= $title ?></h4>
+						<p class="card-description">
+							<!-- Button trigger modal -->
+							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+								Add
+							</button>
+						</p>
+						<div class="table-responsive pt-3">
+							<table class="table table-bordered">
+								<thead>
+									<tr>
+										<th>
+											#
+										</th>
+										<th>
+											Nama Kategori
+										</th>
+										<th>
+											Aksi
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $no = 1;
+									foreach ($kategori as $key => $value) { ?>
+										<tr>
+											<td>
+												<?= $no++ ?>
+											</td>
+											<td>
+												<?= $value->nama_kategori ?>
+											</td>
+											<td>
+												<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?= $value->id_kategori ?>">
+													Edit
+												</button>
+												<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#hapus<?= $value->id_kategori ?>">
+													Hapus
+												</button>
+											</td>
+										</tr>
+									<?php } ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLongTitle">Tambah Kategori</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<form action="<?= base_url('master_produk/add') ?>" method="POST">
+					<div class="modal-body">
+						<input type="text" name="nama_kategori" class="form-control" placeholder="Nama Kategori">
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<button type="submit" class="btn btn-primary">Save</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal edit -->
+	<?php foreach ($kategori as $key => $value) { ?>
+		<div class="modal fade" id="edit<?= $value->id_kategori ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLongTitle">Tambah Kategori</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<form action="<?= base_url('master_produk/update/' . $value->id_kategori) ?>" method="POST">
+						<div class="modal-body">
+							<input type="text" name="nama_kategori" value="<?= $value->nama_kategori ?>" class="form-control" placeholder="Nama Kategori">
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="submit" class="btn btn-primary">Save</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
+
+	<!-- Modal Hapus-->
+	<?php foreach ($kategori as $key => $value) { ?>
+		<div class="modal fade" id="hapus<?= $value->id_kategori ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalLongTitle">Tambah Kategori</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<h5>Apakah Anda Yakin Akan hapus Kategori</h5>
+						<h3><?= $value->nama_kategori ?> ???</h3>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						<a href="<?= base_url('master_produk/delete/' . $value->id_kategori) ?>" class="btn btn-primary">Save</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
