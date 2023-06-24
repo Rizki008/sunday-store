6bd658c73f0fecff4ff05eeb01a181989e67a027
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-24 18:27:12 +0700

    transaksi masuk frontend

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -15,8 +15,33 @@ class Transaksi extends CI_Controller
 	{
 		$data = array(
 			'title' => 'Data Transaksi',
+			'masuk' => $this->m_mastertransaksi->masuk(),
+			'proses' => $this->m_mastertransaksi->proses(),
+			'kirim' => $this->m_mastertransaksi->kirim(),
+			'selesai' => $this->m_mastertransaksi->selesai(),
 			'isi' => 'backend/transaksi/v_transaksi'
 		);
 		$this->load->view('backend/v_wrapper', $data, FALSE);
+	}
+
+	public function detail($id_pesanan)
+	{
+		$data = array(
+			'title' => 'Detail Pesanan',
+			'detail' => $this->m_mastertransaksi->detailpesanan($id_pesanan),
+			'isi' => 'backend/transaksi/v_detail'
+		);
+		$this->load->view('backend/v_wrapper', $data, FALSE);
+	}
+
+	public function konfirmasi($id_pesanan)
+	{
+		$data = array(
+			'id_pesanan' => $id_pesanan,
+			'status_order' => 3
+		);
+		$this->m_mastertransaksi->statuspesan($data);
+		$this->session->set_flashdata('pesan', 'Berhasil Dikonfirmasi');
+		redirect('transaksi');
 	}
 }
