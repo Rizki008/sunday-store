f15435922bcd340bdf5e35fb0b9d89eafc2599d1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-06-24 18:24:09 +0700

    ubah view frontend

--- application/views/frontend/v_home.php
+++ application/views/frontend/v_home.php
@@ -26,179 +26,67 @@
 <div class="product-area bg-image-1 pt-100 pb-95">
 	<div class="container">
 		<div class="featured-product-active hot-flower owl-carousel product-nav">
-			<div class="product-wrapper">
-				<div class="product-img">
-					<a href="product-details.html">
-						<img alt="" src="<?= base_url() ?>frontend/assets/img/product/product-1.jpg">
-					</a>
-					<span>-20%</span>
-					<div class="product-action">
-						<a class="action-wishlist" href="#" title="Wishlist">
-							<i class="ion-android-favorite-outline"></i>
-						</a>
-						<a class="action-cart" href="#" title="Add To Cart">
-							<i class="ion-ios-shuffle-strong"></i>
-						</a>
-						<a class="action-compare" href="#" data-target="#exampleModal" data-toggle="modal" title="Quick View">
-							<i class="ion-ios-search-strong"></i>
-						</a>
-					</div>
-				</div>
-				<div class="product-content text-left">
-					<div class="product-hover-style">
-						<div class="product-title">
-							<h4>
-								<a href="product-details.html">Nature Close Tea</a>
-							</h4>
+			<?php if (count($produk) > 0) : ?>
+				<?php foreach ($produk as $value) : ?>
+					<?php echo form_open('belanja/add');
+					echo form_hidden('id', $value->id_produk);
+					echo form_hidden('name', $value->nama_produk);
+					echo form_hidden('stock', $value->stok);
+					echo form_hidden('netto', $value->berat);
+					echo form_hidden('price', $value->harga - ($value->diskon / 100 * $value->harga));
+					echo form_hidden('picture', $value->foto);
+					echo form_hidden('qty', 1);
+					echo form_hidden('redirect_page', str_replace('index.php/', '', current_url()));
+					?>
+					<div class="product-wrapper">
+						<div class="product-img">
+							<a href="<?= base_url('home/detail/' . $value->id_produk) ?>">
+								<img alt="" src="<?= base_url('assets/produk/' . $value->foto) ?>">
+							</a>
+							<?php if ($value->diskon == 0) { ?>
+							<?php } elseif ($value->diskon > 1) { ?>
+								<span>-<?= $value->diskon ?>%</span>
+							<?php } ?>
+							<div class="product-action">
+								<!-- <a class="action-wishlist" href="#" title="Wishlist">
+									<i class="ion-android-favorite-outline"></i>
+								</a>
+								<a class="action-cart" href="#" title="Add To Cart">
+									<i class="ion-ios-shuffle-strong"></i>
+								</a> -->
+								<a class="action-compare" href="#" data-target="#exampleModal" data-toggle="modal" title="Quick View">
+									<i class="ion-ios-search-strong"></i>
+								</a>
+							</div>
 						</div>
-						<div class="cart-hover">
-							<h4><a href="product-details.html">+ Add to cart</a></h4>
-						</div>
-					</div>
-					<div class="product-price-wrapper">
-						<span>$100.00 -</span>
-						<span class="product-price-old">$120.00 </span>
-					</div>
-				</div>
-			</div>
-			<div class="product-wrapper">
-				<div class="product-img">
-					<a href="product-details.html">
-						<img alt="" src="<?= base_url() ?>frontend/assets/img/product/product-2.jpg">
-					</a>
-					<div class="product-action">
-						<a class="action-wishlist" href="#" title="Wishlist">
-							<i class="ion-android-favorite-outline"></i>
-						</a>
-						<a class="action-cart" href="#" title="Add To Cart">
-							<i class="ion-ios-shuffle-strong"></i>
-						</a>
-						<a class="action-compare" href="#" data-target="#exampleModal" data-toggle="modal" title="Quick View">
-							<i class="ion-ios-search-strong"></i>
-						</a>
-					</div>
-				</div>
-				<div class="product-content text-left">
-					<div class="product-hover-style">
-						<div class="product-title">
-							<h4>
-								<a href="product-details.html">Pink wave Cup</a>
-							</h4>
-						</div>
-						<div class="cart-hover">
-							<h4><a href="product-details.html">+ Add to cart</a></h4>
+						<div class="product-content text-left">
+							<div class="product-hover-style">
+								<div class="product-title">
+									<h4>
+										<a href="<?= base_url('home/detail/' . $value->id_produk) ?>"><?= $value->nama_produk ?></a>
+									</h4>
+								</div>
+								<div class="cart-hover">
+									<h4>
+										<button type="submit" class="btn btn-success">+ Add to cart</button>
+										<!-- <a href="<?= base_url('belanja/add/' . $value->id_produk) ?>">+ Add to cart</a> -->
+									</h4>
+								</div>
+							</div>
+							<div class="product-price-wrapper">
+								<?php if ($value->diskon == 0) { ?>
+									<span>Rp. <?= number_format($value->harga), 0 ?> -</span>
+								<?php } elseif ($value->diskon > 1) { ?>
+									<span>Rp. <?= number_format($value->harga - ($value->diskon / 100 * $value->harga)), 0 ?> -</span>
+									<span class="product-price-old">Rp. <?= number_format($value->harga), 0 ?> </span>
+								<?php } ?>
+							</div>
 						</div>
 					</div>
-					<div class="product-price-wrapper">
-						<span>$100.00 -</span>
-						<span class="product-price-old">$120.00 </span>
-					</div>
-				</div>
-			</div>
-			<div class="product-wrapper">
-				<div class="product-img">
-					<a href="product-details.html">
-						<img alt="" src="<?= base_url() ?>frontend/assets/img/product/product-3.jpg">
-					</a>
-					<span>-50%</span>
-					<div class="product-action">
-						<a class="action-wishlist" href="#" title="Wishlist">
-							<i class="ion-android-favorite-outline"></i>
-						</a>
-						<a class="action-cart" href="#" title="Add To Cart">
-							<i class="ion-ios-shuffle-strong"></i>
-						</a>
-						<a class="action-compare" href="#" data-target="#exampleModal" data-toggle="modal" title="Quick View">
-							<i class="ion-ios-search-strong"></i>
-						</a>
-					</div>
-				</div>
-				<div class="product-content text-left">
-					<div class="product-hover-style">
-						<div class="product-title">
-							<h4>
-								<a href="product-details.html">Tea and Chai</a>
-							</h4>
-						</div>
-						<div class="cart-hover">
-							<h4><a href="product-details.html">+ Add to cart</a></h4>
-						</div>
-					</div>
-					<div class="product-price-wrapper">
-						<span>$100.00 -</span>
-						<span class="product-price-old">$120.00 </span>
-					</div>
-				</div>
-			</div>
-			<div class="product-wrapper">
-				<div class="product-img">
-					<a href="product-details.html">
-						<img alt="" src="<?= base_url() ?>frontend/assets/img/product/product-4.jpg">
-					</a>
-					<div class="product-action">
-						<a class="action-wishlist" href="#" title="Wishlist">
-							<i class="ion-android-favorite-outline"></i>
-						</a>
-						<a class="action-cart" href="#" title="Add To Cart">
-							<i class="ion-ios-shuffle-strong"></i>
-						</a>
-						<a class="action-compare" href="#" data-target="#exampleModal" data-toggle="modal" title="Quick View">
-							<i class="ion-ios-search-strong"></i>
-						</a>
-					</div>
-				</div>
-				<div class="product-content text-left">
-					<div class="product-hover-style">
-						<div class="product-title">
-							<h4>
-								<a href="product-details.html">Pearly Everlasting</a>
-							</h4>
-						</div>
-						<div class="cart-hover">
-							<h4><a href="product-details.html">+ Add to cart</a></h4>
-						</div>
-					</div>
-					<div class="product-price-wrapper">
-						<span>$100.00 -</span>
-						<span class="product-price-old">$120.00 </span>
-					</div>
-				</div>
-			</div>
-			<div class="product-wrapper">
-				<div class="product-img">
-					<a href="product-details.html">
-						<img alt="" src="<?= base_url() ?>frontend/assets/img/product/product-5.jpg">
-					</a>
-					<span>-30%</span>
-					<div class="product-action">
-						<a class="action-wishlist" href="#" title="Wishlist">
-							<i class="ion-android-favorite-outline"></i>
-						</a>
-						<a class="action-cart" href="#" title="Add To Cart">
-							<i class="ion-ios-shuffle-strong"></i>
-						</a>
-						<a class="action-compare" href="#" data-target="#exampleModal" data-toggle="modal" title="Quick View">
-							<i class="ion-ios-search-strong"></i>
-						</a>
-					</div>
-				</div>
-				<div class="product-content text-left">
-					<div class="product-hover-style">
-						<div class="product-title">
-							<h4>
-								<a href="product-details.html">Que herbal Tea</a>
-							</h4>
-						</div>
-						<div class="cart-hover">
-							<h4><a href="product-details.html">+ Add to cart</a></h4>
-						</div>
-					</div>
-					<div class="product-price-wrapper">
-						<span>$100.00 -</span>
-						<span class="product-price-old">$120.00 </span>
-					</div>
-				</div>
-			</div>
+					<?php echo form_close() ?>
+				<?php endforeach; ?>
+			<?php else : ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
