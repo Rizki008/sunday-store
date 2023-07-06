c60381a5157cfc0e8f0cf611df1ee717913c53d0
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-06 11:44:39 +0700

    model analisis dan grafik

---
+++ application/models/M_analisis.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_analisis extends CI_Model
+{
+	public function variabel($date)
+	{
+		$data['all'] = $this->db->query("SELECT COUNT(pesanan.id_pesanan) as frequency, DATEDIFF('" . $date . "', MAX(tanggal_pesanan)) as recency, SUM(qty) as monetary, id_produk FROM `pesanan` JOIN detail_pesanan ON pesanan.id_pesanan = detail_pesanan.id_pesanan GROUP BY id_produk")->result();
+		$data['limit'] = $this->db->query("SELECT COUNT(pesanan.id_pesanan) as frequency, DATEDIFF('" . $date . "', MAX(tanggal_pesanan)) as recency, SUM(qty) as monetary, id_produk FROM `pesanan` JOIN detail_pesanan ON pesanan.id_pesanan = detail_pesanan.id_pesanan GROUP BY id_produk LIMIT 3")->result();
+		return $data;
+	}
+
+	public function grafik()
+	{
+		return $this->db->query("SELECT nama_produk,status,qty,
+								CASE
+									WHEN status = 1 THEN 'Laku'
+									WHEN status = 2 THEN 'Tidak Laku'
+									ELSE 'Kurang laku'
+								END AS hasils
+								FROM produk
+								LEFT JOIN detail_pesanan ON detail_pesanan.id_produk=produk.id_produk
+								GROUP BY detail_pesanan.id_produk
+								ORDER BY status=1 DESC")->result();
+	}
+}
+
+/* End of file M_analisis.php */
