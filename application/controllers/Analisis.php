cb853049310970438c16e0f61c5bf6d3e8a4765f
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-06 11:45:15 +0700

    proses analisis k-means

---
+++ application/controllers/Analisis.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Analisis extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_analisis');
+	}
+
+	// List all your items
+	public function index($offset = 0)
+	{
+		$data = array(
+			'title' => 'Analisis K-Means',
+			'grafik' => $this->m_analisis->grafik(),
+			'isi' => 'backend/analisis/v_analisis'
+		);
+		$this->load->view('backend/v_wrapper', $data, FALSE);
+	}
+
+	// Add a new item
+	public function pimpinan()
+	{
+		$data = array(
+			'title' => 'Analisis K-Means',
+			'grafik' => $this->m_analisis->grafik(),
+			'isi' => 'pimpinan/analisis/v_analisis'
+		);
+		$this->load->view('pimpinan/v_wrapper', $data, FALSE);
+	}
+
+	//Update one item
+	public function update($id = NULL)
+	{
+	}
+
+	//Delete one item
+	public function delete($id = NULL)
+	{
+	}
+}
+
+/* End of file Analisis.php */
