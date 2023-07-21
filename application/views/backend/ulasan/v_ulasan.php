e284658ce2ed64da8a5b96eb336b3ba5f89cd17c
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-21 08:17:19 +0700

    view ulasan

---
+++ application/views/backend/ulasan/v_ulasan.php
@@ -0,0 +1,53 @@
+<div class="main-panel">
+	<div class="content-wrapper">
+		<div class="row">
+			<div class="col-lg-12 grid-margin stretch-card">
+				<div class="card">
+					<div class="card-body">
+						<h4 class="card-title"><?= $title ?></h4>
+						<p class="card-description">
+						</p>
+						<div class="table-responsive">
+							<table id="example" class="table table-striped table-bordered" style="width:100%">
+								<thead>
+									<tr>
+										<th>
+											Nama Pelanggan
+										</th>
+										<th>
+											Nama Produk
+										</th>
+										<th>
+											Ulasan
+										</th>
+										<th>
+											Tanggal Ulasan
+										</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php $no = 1;
+									foreach ($ulasan as $key => $value) { ?>
+										<tr>
+											<td class="py-1">
+												<?= $value->nama_pelanggan ?>
+											</td>
+											<td>
+												<?= $value->nama_produk ?>
+											</td>
+											<td>
+												<?= $value->ulasan ?>
+											</td>
+											<td>
+												<?= $value->tanggal_ulasan ?>
+											</td>
+										</tr>
+									<?php } ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
