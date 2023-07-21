c3da40621a46e10f14cd291f3c295e25e866f36f
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-07-21 08:17:38 +0700

    menambahkan model ulasan

--- application/models/M_ulasan.php
+++ application/models/M_ulasan.php
@@ -24,6 +24,17 @@ class M_ulasan extends CI_Model
 		// $this->db->where('ulasan.id_produk', $id_produk);
 		// return $this->db->get()->result();
 	}
+
+	public function data_ulasan()
+	{
+		$this->db->select('*');
+		$this->db->from('ulasan');
+		$this->db->join('detail_pesanan', 'detail_pesanan.id_detail = ulasan.id_detail', 'left');
+		$this->db->join('pesanan', 'pesanan.id_pesanan = detail_pesanan.id_pesanan', 'left');
+		$this->db->join('pelanggan', 'pelanggan.id_pelanggan = pesanan.id_pelanggan', 'left');
+		$this->db->join('produk', 'produk.id_produk = ulasan.id_produk', 'left');
+		return $this->db->get()->result();
+	}
 }
 
 /* End of file M_ulasan.php */
